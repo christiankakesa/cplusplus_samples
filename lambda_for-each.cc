e74a09c1a1276cfdb6bae112769e6eef43dc4d0a
Christian Kakesa <christian.kakesa@gmail.com>
2012-10-10 22:06:40 +0200

    update lambda_for-each.cc example

--- lambda_for-each.cc
+++ lambda_for-each.cc
@@ -4,21 +4,24 @@
 
 int main() {
 	std::vector<std::string> data{"first", "second", "third"};
-	// Print data vector before processing
-	std::cout << "before:" << std::endl << " - ";
-	for (const auto d : data) {
-		std::cout << d << " ";
-	}
+	std::string one = "one!!";
+	std::string two = "two";
+	std::string three = "three";
 	std::cout << std::endl;
 	// Process data vector with lambda function
-	for_each(begin(data), end(data), [](std::string &item) {
-		item += "_append";
+	for_each(begin(data), end(data), [one, &two, &three](std::string &item) mutable {
+		item  += "_append";
+		one   += "_another value now"; // We use "mutable" for variable passed by copy.
+		two   += "_two";
+		three += "_three";
 	});
 	// Print data vector after processing
 	std::cout << "after:" << std::endl << " - ";
 	for (const auto d : data) {
 		std::cout << d << " ";
 	}
+	std::cout << std::endl;
+	std::cout << " - one: " << one << " - two: " << two << " - three: " << three;
 	std::cout << std::endl;
 	return 0;
 }
