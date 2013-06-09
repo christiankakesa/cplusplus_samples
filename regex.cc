dae53be2d4c5776e7ef7bf79b1c13dc089cce9e6
Christian Kakesa <christian.kakesa@gmail.com>
2013-06-10 01:41:16 +0200

    update regex example

--- regex.cc
+++ regex.cc
@@ -7,7 +7,11 @@ int main() {
     static const std::regex my_regex{R"(\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b)"};
     std::smatch my_matches;
     if (std::regex_search(text, my_matches, my_regex)) {
-      std::cout << "I found email" << std::endl;
+      std::cout << "I found email:" << std::endl;
+      std::copy(begin(my_matches),
+		end(my_matches),
+		std::ostream_iterator<std::string>(std::cout, std::endl));
+      std::cout << std::endl;
     } else {
       std::cout << "No email match!!!" << std::endl;
     }
