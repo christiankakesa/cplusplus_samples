5b7e675e5ba6b9aac01826568daf208101392bb8
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-20 02:21:53 +0100

    Add binary literals and digit separator sample.

---
+++ src/binary_literals.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <bitset>
+
+int main() {
+    std::cout << "[Binary literals an digits separator ]" << std::endl;
+    std::cout << "0b0101'1111'0000'1000: " << 0b0101'1111'0000'1000 << std::endl;
+    return EXIT_SUCCESS;
+}
